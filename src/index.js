a58439faad368c641fc1aa1f875379fd3fcdc3e7
katiadeo <niel.0305@yandex.ru>
2021-02-27 21:56:31 +0300

    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  let config = bracketsConfig.join('').replace(/,/g, '');
+  let stack = [];
+  
+  for (let i = 0; i < str.length; i++) {
+    let element = str[i];
+    let index = config.indexOf(element)
+    // if index is even => open bracket
+    if (index % 2 === 0) {
+              
+      if (element === config[index + 1] && stack[stack.length - 1] === index) {
+          stack.pop();
+      } else if (stack[stack.length - 1] !== index) {
+          stack.push(index)
+      } else {
+          // push OPEN BRACKET onto stack
+          stack.push(index);
+      }
+
+    }
+    // if index is not even => close bracket
+    else {
+    // if (open bracket doesn't equal to "close bracket - 1" (= open bracket) )
+      if (stack.pop() !== index - 1){
+        return false;
+      }
+    }
+          
+  }
+
+  if (stack.length !== 0) {
+      return false;
+  }
+  return true;
 }
